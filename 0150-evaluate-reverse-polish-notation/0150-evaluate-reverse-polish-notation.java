8a48b0b1946c4f55bbea868df4e2e967ad5364cc
nitishgupta5862-ctrl <nitishgupta5862@gmail.com>
2026-08-05 22:52:28 +0530

    Time: 6 ms (87.24%), Space: 45.2 MB (63.46%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,35 +1,61 @@
 class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> s=new Stack<>();
-        for(int i=0;i<tokens.length;i++){
-            String token =tokens[i];
-           switch(token){
-            case "+":{
-             int a=s.pop();
-             int b=s.pop();
-             s.push(a+b);
-             break;
+        // for(int i=0;i<tokens.length;i++){
+        //     String token =tokens[i];
+        //    switch(token){
+        //     case "+":{
+        //      int a=s.pop();
+        //      int b=s.pop();
+        //      s.push(a+b);
+        //      break;
+        //     }
+        //      case "-":{
+        //      int a=s.pop();
+        //      int b=s.pop();
+        //      s.push(b-a);
+        //      break;}
+        //      case"*":{
+        //      int a=s.pop();
+        //      int b=s.pop();
+        //      s.push(a*b);
+        //      break;}
+        //      case "/":{
+        //      int a=s.pop();
+        //      int b=s.pop();
+        //      s.push(b/a);
+        //      break;
+        //    }
+        //    default:{
+        //     s.push(Integer.parseInt(token));
+        //    }
+        // }
+        // }
+        // return s.peek();
+        for(String token :tokens){
+            if(token.equals("+")){
+                int a=s.pop();
+                int b=s.pop();
+                s.push(a+b);
             }
-             case "-":{
-             int a=s.pop();
-             int b=s.pop();
-             s.push(b-a);
-             break;}
-             case"*":{
-             int a=s.pop();
-             int b=s.pop();
-             s.push(a*b);
-             break;}
-             case "/":{
-             int a=s.pop();
-             int b=s.pop();
-             s.push(b/a);
-             break;
-           }
-           default:{
-            s.push(Integer.parseInt(token));
-           }
-        }
+           else if(token.equals("-")){
+                int a=s.pop();
+                int b=s.pop();
+                s.push(b-a);
+            } 
+            else if(token.equals("*")){
+                int a=s.pop();
+                int b=s.pop();
+                s.push(b*a);
+            } 
+              else if(token.equals("/")){
+                int a=s.pop();
+                int b=s.pop();
+                s.push(b/a);
+            } 
+            else{
+                s.push(Integer.parseInt(token));
+            }
         }
         return s.peek();
     }
