a8635ddcee4f0b8b2a0148e47c47c6bcc1db1e49
nitishgupta5862-ctrl <nitishgupta5862@gmail.com>
2026-08-10 18:41:39 +0530

    Time: 2 ms (97.21%), Space: 44.4 MB (94.7%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,24 +1,61 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        Stack<Integer> st=new Stack<>();
-        st.push(-1);
+        // Stack<Integer> st=new Stack<>(); //O(n)and O(1)
+        // st.push(-1);
+        // int max=0;
+        // for(int i=0;i<s.length();i++){
+        //     char ch=s.charAt(i);
+        //     if(ch=='('){
+        //         st.push(i);
+        //     }
+        //     else{
+        //         st.pop();
+        //         if(st.isEmpty()){
+        //             st.push(i);
+        //         }
+        //         else{
+        //             int len=i-st.peek();
+        //             max=Math.max(len,max);
+        //         }
+        //     }
+        // }
+        // return max;
+        int close=0;
+        int open =0;
         int max=0;
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             if(ch=='('){
-                st.push(i);
+                open++;
             }
             else{
-                st.pop();
-                if(st.isEmpty()){
-                    st.push(i);
-                }
-                else{
-                    int len=i-st.peek();
-                    max=Math.max(len,max);
-                }
+                close++;
+            }
+            if(open==close){
+                int len =open +close;
+                max=Math.max(len,max);
+            }
+            else if(close>open){
+                open =close=0;
             }
         }
-        return max;
+        open=close=0;
+        for(int i=s.length()-1;i>=0;i--){
+            char ch=s.charAt(i);
+            if(ch=='('){
+                open++;
+            }
+            else{
+                close++;
+            }
+            if(open==close){
+                int len =open +close;
+                max=Math.max(len,max);
+            }
+            else if(open>close){
+                open =close=0;
+            }
+        }
+   return max;
     }
 }
