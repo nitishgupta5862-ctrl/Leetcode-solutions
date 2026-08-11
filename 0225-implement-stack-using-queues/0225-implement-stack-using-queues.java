b046b0bfddc50e995c42cf3406f2fd0e63230aa7
nitishgupta5862-ctrl <nitishgupta5862@gmail.com>
2026-08-12 00:38:47 +0530

    Time: 1 ms (81.59%), Space: 42.9 MB (34.97%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,77 +1,118 @@
 class MyStack {
    Queue<Integer> q1=new LinkedList<>();
-   Queue<Integer> q2=new LinkedList();
+  // Queue<Integer> q2=new LinkedList();
     public MyStack() {
         
     }
     
-    public void push(int x) {
-        if(q1.isEmpty() && q2.isEmpty()){
-        q1.add(x);
-    }
+//     public void push(int x) {
+//         if(q1.isEmpty() && q2.isEmpty()){
+//         q1.add(x);
+//     }
 
-       else if(!q1.isEmpty()){
-            q1.add(x);
-        }
-        else{
-            q2.add(x);
-        }
+//        else if(!q1.isEmpty()){
+//             q1.add(x);
+//         }
+//         else{
+//             q2.add(x);
+//         }
+//     }
+    
+//     public int pop() {
+//         if(empty()){
+//             return -1;
+//         }
+//         int top=-1;
+//         if(!q1.isEmpty()){
+//             while(!q1.isEmpty()){
+//                 top=q1.remove();
+//                 if(q1.isEmpty()){
+//                     break;
+//                 }
+//                 q2.add(top);
+//             }
+//         }
+//         else{
+//             while(!q2.isEmpty()){
+//                 top=q2.remove();
+//                 if(q2.isEmpty()){
+//                     break;
+//                 }
+//                 q1.add(top);
+
+//             }
+//         }
+//         return top;
+//     }
+    
+//     public int top() {
+//          if(empty()){
+//             return -1;
+//         }
+//         int top=-1;
+//         if(!q1.isEmpty()){
+//             while(!q1.isEmpty()){
+//                 top=q1.remove();
+//                 q2.add(top);
+//             }
+//         }
+//         else{
+//             while(!q2.isEmpty()){
+//                 top=q2.remove();
+//                 q1.add(top);
+
+//             }
+//         }
+//         return top;
+        
+//     }
+    
+//     public boolean empty() {
+//         return q1.isEmpty()&&q2.isEmpty();
+        
+//     }
+// }
+//   Queue<Integer> q1=new LinkedList<>();
+//    Queue<Integer> q2=new LinkedList();
+//     public MyStack() {
+        
+//     }
+    
+//     public void push(int x) {
+//         if(q1.isEmpty() && q2.isEmpty()){
+//         q1.add(x);
+//     }
+
+//        else if(!q1.isEmpty()){
+//             q1.add(x);
+//         }
+//         else{
+//             q2.add(x);
+//         }
+//     }
+public void push(int x){  //o(n)
+    q1.add(x);
+    int n=q1.size();
+    for(int i=1;i<n;i++){
+        q1.add(q1.remove());
     }
+}
     
     public int pop() {
-        if(empty()){
-            return -1;
-        }
-        int top=-1;
-        if(!q1.isEmpty()){
-            while(!q1.isEmpty()){
-                top=q1.remove();
-                if(q1.isEmpty()){
-                    break;
-                }
-                q2.add(top);
-            }
-        }
-        else{
-            while(!q2.isEmpty()){
-                top=q2.remove();
-                if(q2.isEmpty()){
-                    break;
-                }
-                q1.add(top);
-
-            }
-        }
-        return top;
+    return q1.remove();
     }
     
     public int top() {
-         if(empty()){
-            return -1;
-        }
-        int top=-1;
-        if(!q1.isEmpty()){
-            while(!q1.isEmpty()){
-                top=q1.remove();
-                q2.add(top);
-            }
-        }
-        else{
-            while(!q2.isEmpty()){
-                top=q2.remove();
-                q1.add(top);
-
-            }
-        }
-        return top;
+      return  q1.peek();
         
     }
     
     public boolean empty() {
-        return q1.isEmpty()&&q2.isEmpty();
+        return q1.isEmpty();
         
     }
 }
+    
     
 /**
  * Your MyStack object will be instantiated and called as such:
