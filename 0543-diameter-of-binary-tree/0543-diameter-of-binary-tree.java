202309238cf5c47609e7c16b2c52768139696f47
nitishgupta5862-ctrl <nitishgupta5862@gmail.com>
2026-08-22 14:09:13 +0530

    Time: 1 ms (44.99%), Space: 46.4 MB (98.93%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,24 +14,46 @@
  * }
  */
 class Solution {
-    public int height(TreeNode root){
-        if(root==null){
-            return 0;
+    class info{
+        int dia;
+        int ht;
+        public info (int dia,int ht){
+            this.dia=dia;
+            this.ht=ht;
         }
-        int lh=height(root.left);
-        int rh=height(root.right);
-        return Math.max(lh,rh)+1;
+
+
     }
-    public int diameterOfBinaryTree(TreeNode root) {
-        if(root==null){
-            return 0;
-        }
-        int lh=height(root.left);
-        int rh=height(root.right);
-        int ld=diameterOfBinaryTree(root.left);
-        int rd=diameterOfBinaryTree(root.right);
-        int selfdia=lh+rh;
-        return Math.max(selfdia,Math.max(ld,rd));
+    //public int height(TreeNode root){
+    //     if(root==null){      //o(n*2)
+    //         return 0;
+    //     }
+    //     int lh=height(root.left);
+    //     int rh=height(root.right);
+    //     return Math.max(lh,rh)+1;
+    // }
+    public info diameter(TreeNode root) {
+    //     if(root==null){
+    //         return 0;
+    //     }
+    //     int lh=height(root.left);
+    //     int rh=height(root.right);
+    //     int ld=diameterOfBinaryTree(root.left);
+    //     int rd=diameterOfBinaryTree(root.right);
+    //     int selfdia=lh+rh;
+    //     return Math.max(selfdia,Math.max(ld,rd));
+    if(root==null){
+        return new info(0,0);
+    }
+    info infoleft=diameter(root.left);
+    info inforight=diameter(root.right);
+    int dia=Math.max(Math.max(infoleft.dia,inforight.dia),infoleft.ht+inforight.ht);
+    int ht=Math.max(infoleft.ht,inforight.ht)+1;
+    return new info(dia,ht);
+
         
+    }
+    public int diameterOfBinaryTree(TreeNode root){
+        return diameter(root).dia;
     }
 }
