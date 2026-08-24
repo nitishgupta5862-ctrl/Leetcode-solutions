63656cbddce0e9e7e3980c7a39f6494fe891a089
nitishgupta5862-ctrl <nitishgupta5862@gmail.com>
2026-08-24 10:22:11 +0530

    Time: 1 ms (100%), Space: 48.1 MB (39.84%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,28 +1,43 @@
 class Solution {
     public int[] singleNumber(int[] arr) {
-        ArrayList<Integer> list=new ArrayList<>();
-        Arrays.sort(arr);
-        if(arr.length<=2){
-            return arr;
-        }
-        if(arr[0]!=arr[1]){
-            list.add(arr[0]);
-        }
-        if(arr[arr.length-1]!=arr[arr.length-2]){
-            list.add(arr[arr.length-1]);
-        }
-        for(int i=1;i<arr.length-1;i++){
-            if(arr[i]!=arr[i+1]&&arr[i]!=arr[i-1]){
-                list.add(arr[i]);
-            }
-        }
-        int[] nums = new int[list.size()];
+//         ArrayList<Integer> list=new ArrayList<>();
+//         Arrays.sort(arr);
+//         if(arr.length<=2){
+//             return arr;
+//         }
+//         if(arr[0]!=arr[1]){
+//             list.add(arr[0]);
+//         }
+//         if(arr[arr.length-1]!=arr[arr.length-2]){
+//             list.add(arr[arr.length-1]);
+//         }
+//         for(int i=1;i<arr.length-1;i++){
+//             if(arr[i]!=arr[i+1]&&arr[i]!=arr[i-1]){
+//                 list.add(arr[i]);
+//             }
+//         }
+//         int[] nums = new int[list.size()];
 
-        for (int i = 0; i < list.size(); i++) {
-           nums[i] = list.get(i);
-}
-      return nums;
-
+//         for (int i = 0; i < list.size(); i++) {
+//            nums[i] = list.get(i);
+// }
+//       return nums;
+     int ans=0;
+     for(int i=0;i<arr.length;i++){
+        ans ^=arr[i];
+     }
+     int diffbit=ans &(-ans);
+     int a=0;
+     int b=0;
+     for(int i=0;i<arr.length;i++){
+        if((arr[i]&diffbit)==0){
+            a ^=arr[i];
+        }
+        else{
+            b ^=arr[i];
+        }
+     }
+     return new int[]{a,b};
 
     }
 }
