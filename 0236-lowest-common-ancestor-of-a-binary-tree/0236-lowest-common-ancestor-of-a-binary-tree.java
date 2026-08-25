b43108add4dac5669f107773178d4f98eb400ecd
nitishgupta5862-ctrl <nitishgupta5862@gmail.com>
2026-08-26 01:03:55 +0530

    Time: 15 ms (90.9%), Space: 69.8 MB (60.99%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,36 +8,50 @@
  * }
  */
 class Solution {
-    public boolean getpath(TreeNode root,TreeNode n,ArrayList<TreeNode>path){
-        if(root==null){
-            return false;
-        }
-        path.add(root);
-        if(root.val==n.val){
-            return true;
-        }
-       boolean leftfound= getpath(root.left,n,path);
-       boolean rightfound=getpath(root.right,n,path);
-       if(leftfound ||rightfound){
-        return true;
-       }
-       path.remove(path.size()-1);
-       return false;
+    // public boolean getpath(TreeNode root,TreeNode n,ArrayList<TreeNode>path){
+    //     if(root==null){
+    //         return false;
+    //     }
+    //     path.add(root);
+    //     if(root.val==n.val){
+    //         return true;
+    //     }
+    //    boolean leftfound= getpath(root.left,n,path);
+    //    boolean rightfound=getpath(root.right,n,path);
+    //    if(leftfound ||rightfound){
+    //     return true;
+    //    }
+    //    path.remove(path.size()-1);
+    //    return false;
 
-    }
+    // }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ArrayList<TreeNode> path1=new ArrayList<>();
-        ArrayList<TreeNode> path2=new ArrayList<>();
-        getpath(root,p,path1);
-        getpath(root,q,path2);
-        int i=0;
-        for(;i<path1.size()&&i<path2.size();i++){
-            if(path1.get(i)!=path2.get(i)){
-                break;
-            }
-        }
-        TreeNode lca=path1.get(i-1);
-        return lca;
+        // ArrayList<TreeNode> path1=new ArrayList<>();
+        // ArrayList<TreeNode> path2=new ArrayList<>();
+        // getpath(root,p,path1);
+        // getpath(root,q,path2);
+        // int i=0;
+        // for(;i<path1.size()&&i<path2.size();i++){
+        //     if(path1.get(i)!=path2.get(i)){
+        //         break;
+        //     }
+        // }
+        // TreeNode lca=path1.get(i-1);
+        // return lca;
         
+      if(root==null||root.val==p.val||root.val==q.val){
+        return root;
+      }
+      TreeNode leftlca=lowestCommonAncestor(root.left,p,q);
+      TreeNode rightlca=lowestCommonAncestor(root.right,p,q);
+
+      //ek valid ek null ho tb
+      if(leftlca==null){
+        return rightlca;
+      }
+      if(rightlca==null){
+        return leftlca;
+      }
+        return root;
     }
 }
