a17f4bec6cde4fea941b57ceb5a6d69bc4114083
nitishgupta5862-ctrl <nitishgupta5862@gmail.com>
2026-09-17 22:23:44 +0530

    Time: 3 ms (95.86%), Space: 52.2 MB (54.72%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -19,7 +19,32 @@ class Solution {
         // return maxlen;
 
 
-        //average wala case
+                                             // average wala case
+        int zeroes=0;
+        int left=0;
+        int right=0;
+        int maxlen=0;
+        while(right<nums.length){
+            if(nums[right]==0){
+                zeroes++;
+            }
+            while(zeroes>k){
+                if(nums[left]==0){
+                    zeroes--;    //zero ko remove karenge 
+                }
+                left++;
+            }
+            if(zeroes<=k){
+                maxlen=Math.max(maxlen,right-left+1);
+    
+            }
+            right++;
+            
+        }
+        return maxlen;
+
+
+        //                                             //best case
         // int zeroes=0;
         // int left=0;
         // int right=0;
@@ -28,7 +53,7 @@ class Solution {
         //     if(nums[right]==0){
         //         zeroes++;
         //     }
-        //     while(zeroes>k){
+        //     if(zeroes>k){
         //         if(nums[left]==0){
         //             zeroes--;    //zero ko remove karenge 
         //         }
@@ -36,32 +61,9 @@ class Solution {
         //     }
         //     if(zeroes<=k){
         //         maxlen=Math.max(maxlen,right-left+1);
-        //         right++;
         //     }
+        //     right++;          
         // }
-        // return maxlen;
-
-
-                                                    //best case
-                   int zeroes=0;
-        int left=0;
-        int right=0;
-        int maxlen=0;
-        while(right<nums.length){
-            if(nums[right]==0){
-                zeroes++;
-            }
-            if(zeroes>k){
-                if(nums[left]==0){
-                    zeroes--;    //zero ko remove karenge 
-                }
-                left++;
-            }
-            if(zeroes<=k){
-                maxlen=Math.max(maxlen,right-left+1);
-            }
-            right++;
-        }
-        return maxlen;                                  
+        // return maxlen;                                  
     }
 }
