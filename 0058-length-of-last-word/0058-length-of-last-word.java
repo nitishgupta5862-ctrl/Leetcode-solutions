60b6dad6039a644cd044f6b0f844d90075ac4328
nitishgupta5862-ctrl <nitishgupta5862@gmail.com>
2026-09-19 01:28:28 +0530

    Time: 2 ms (8.65%), Space: 43.4 MB (5.98%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,14 +1,25 @@
-class Solution {
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+        // int count=0;
+        // int i=s.length()-1;
+        // while(i>=0&&s.charAt(i)==' '){
+        //     i--;
+        // }
+        // while(i>=0&&s.charAt(i)!=' '){
+        //     count++;
+        //     i--;
+        // }
+        // return count;
+  //  }
+//}
+        //method 2
+ class Solution {
     public int lengthOfLastWord(String s) {
-        int count=0;
-        int i=s.length()-1;
-        while(i>=0&&s.charAt(i)==' '){
-            i--;
-        }
-        while(i>=0&&s.charAt(i)!=' '){
-            count++;
-            i--;
-        }
-        return count;
+
+        String[] arr = s.trim().split("\\s+");//|| iska use more than one space ko remove karne ke liye
+
+        return arr[arr.length - 1].length();
+ 
     }
 }
+    
