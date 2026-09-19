374101a916b4e9fb98255eb142406acec557d6b1
nitishgupta5862-ctrl <nitishgupta5862@gmail.com>
2026-09-20 00:58:01 +0530

    Time: 3 ms (42.87%), Space: 43 MB (71.28%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,27 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char sum=0;
+        char s1[]=s.toCharArray();
+        char s2[]=t.toCharArray();
+        // for(int i=0;i<s1.length;i++){
+        //     sum ^=s1[i];
+        // }
+        // for(int i=0;i<s2.length;i++){
+        //     sum ^=s2[i];
+        // }
+        // return sum;
+
+        //method 2
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+        int i=0;
+        while(i<Math.min(s1.length,s2.length)){
+            if(s1[i]!=s2[i]){
+                return s2[i];
+            }
+            i++;
+        }
+        return s2[i];
+        
+    }
+}
