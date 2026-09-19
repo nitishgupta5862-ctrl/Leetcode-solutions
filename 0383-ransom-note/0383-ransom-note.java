1f1f01c297a61e76aac23f1b2721746ccff1a4b7
nitishgupta5862-ctrl <nitishgupta5862@gmail.com>
2026-09-19 23:32:45 +0530

    Time: 20 ms (5.11%), Space: 47.3 MB (7.26%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,40 +1,47 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int freq[]=new int[26];
-        for(int i=0;i<magazine.length();i++){
-            freq[magazine.charAt(i)-'a']++;
-        }
-        for(int i=0;i<ransomNote.length();i++){
-            freq[ransomNote.charAt(i)-'a']--;
-            if(freq[ransomNote.charAt(i)-'a']<0){
-                return false;
-            }
-
-        }
-        return true;
-        // HashMap<Character,Integer> mmap=new HashMap<>();
+        // int freq[]=new int[26];
         // for(int i=0;i<magazine.length();i++){
-        //     char ch=magazine.charAt(i);
-        //     if(mmap.containsKey(ch)){
-        //         int mfreq=mmap.get(ch);
-        //         mmap.put(ch,mfreq+1);
-        //     }else{
-        //         mmap.put(ch,1);
-        //     }
+        //     freq[magazine.charAt(i)-'a']++;
         // }
-        //     HashMap<Character,Integer> rmap=new HashMap<>();
         // for(int i=0;i<ransomNote.length();i++){
-        //     char ch=ransomNote.charAt(i);
-        //     if(rmap.containsKey(ch)){
-        //         int rfreq=rmap.get(ch);
-        //         rmap.put(ch,rfreq+1);
-        //     }else{
-        //         rmap.put(ch,1);
-        //     }
-        //     if(mfreq<rfreq){
+        //     freq[ransomNote.charAt(i)-'a']--;
+        //     if(freq[ransomNote.charAt(i)-'a']<0){
         //         return false;
         //     }
+
         // }
         // return true;
+        HashMap<Character,Integer> mmap=new HashMap<>();
+        for(int i=0;i<magazine.length();i++){
+            char ch=magazine.charAt(i);
+            if(mmap.containsKey(ch)){
+                int mfreq=mmap.get(ch);
+                mmap.put(ch,mfreq+1);
+            }else{
+                mmap.put(ch,1);
+            }
+        }
+            HashMap<Character,Integer> rmap=new HashMap<>();
+        for(int i=0;i<ransomNote.length();i++){
+            char ch=ransomNote.charAt(i);
+            if(!mmap.containsKey(ch)){
+                return false;
+            }
+            if(rmap.containsKey(ch)){
+                int rfreq=rmap.get(ch)+1;
+                rmap.put(ch,rfreq);
+                if(rfreq>mmap.get(ch)){
+                    return false;
+                }
+            }else{
+                rmap.put(ch,1);
+               if (1 > mmap.get(ch)) {
+       return false;
+            }
+            
+              }
+    }
+        return true;
    }
 }
